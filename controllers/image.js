36d3c68de768d3992c85636c1c2b6cbe5dcf3ff9
Zachary Kohs <zachary.c.kohs@gmail.com>
2024-02-10 20:03:40 -0500

    put the call to Clarifai on the server

--- controllers/image.js
+++ controllers/image.js
@@ -1,15 +1,74 @@
+async function processingImageAPI (image) {
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    // In this section, we set the user authentication, user and app ID, model details, and the URL
+    // of the image we want as an input. Change these strings to run your own example.
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = 'ef95d17a1766421b9335c5eea1684a77';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'zachary_c_kohs';
+    const APP_ID = 'smartbrain';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+    const IMAGE_URL = image;
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    // Data to send to the Clarifai API
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                        // "base64": IMAGE_BYTES_STRING
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    // Fetch From Clarifai API
+    const apiFetch = await fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+    const response = await apiFetch.json()
+    
+    // return the regions of the faces
+    return response.outputs[0].data.regions
+    
+}
+
 const handleImage = (req, res, db) => {
     const {id} = req.body
-    db('users')
-    .where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0].entries)
-    })
-    .catch(err => {
-        res.status(400).json('Unable to get entries.')
-    })
+    processingImageAPI(req.body.image).then(imageRegions => {
+        db('users')
+        .where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json({
+                entries: entries[0].entries, 
+                regions: imageRegions
+            })
+        })
+        .catch(err => {
+            res.status(400).json('Unable to get entries.')
+        })
+    }) 
 }
 
 module.exports = {
--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,9 @@
 const handleRegister = (req, res, db, bcrypt) => {
     const { name, email, password } = req.body
+    if (!email || !name || !password) {
+        return res.status(400).json("Incorrect form submission")
+    }
     const hash = bcrypt.hashSync(password)
-    // 
     db.transaction(trx => {
         trx.insert({
             hash: hash,
