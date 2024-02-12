00bf43fe9a15e75ae4500bf2468eb44be1d0e099
Zachary Kohs <zachary.c.kohs@gmail.com>
2024-02-12 16:13:12 -0500

    deleted unecessary code

--- index.js
+++ index.js
@@ -17,13 +17,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
 const port = process.env.PORT || 3000;
 
 const db = knex({
